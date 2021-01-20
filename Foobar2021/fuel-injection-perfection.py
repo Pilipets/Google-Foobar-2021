f731580e2814c25dddf0788e3e1ce5656821fb3d
Hlib Pylypets <pilipets.gleb@gmail.com>
2021-01-20 03:59:50 +0200

    Added fuel-injection-perfection.
    Signed-off-by: Hlib Pylypets <pilipets.gleb@gmail.com>

--- Foobar2021/fuel-injection-perfection.py
+++ Foobar2021/fuel-injection-perfection.py
@@ -1,1 +1,12 @@
+def solution(n):
+    n, steps = int(n), 0
+    while n > 1:
+        if n & 1:
+            if n & 2 and n != 3: n += 1
+            else: n -= 1
+        else:
+            n >>= 1
+        steps += 1
+    return steps
 
+print(solution('15'))
